a729e992b8f70b49cb8f86435fdd5c29f8fa431c
Augusto Soares <gutosoares@outlook.com>
2018-03-02 21:43:47 -0300

    Edit apidoc for get http method.

---
+++ src/modules/realties/routes/_apidoc.ts
@@ -0,0 +1,14 @@
+/**
+ * @api {get} /realties - Lista todos os imóveis
+ * @apiName RealtiesList
+ * @apiGroup Realties
+ * @apiVersion  1.0.0
+ */
+
+ /**
+ * @api {get} /realties/:id - Lista um imóveis
+ * @apiName RealtiesList
+ * @apiGroup Realties
+ * @apiParam {String} id - ID do imóvel desejado
+ * @apiVersion  1.0.0
+ */
--- src/modules/users/routes/_apidoc.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @api {get} /users Lista todos os usuários
- * @apiName UsersList
- * @apiGroup Users
- * @apiVersion  1.0.0
- * @apiPermission Token
- * @apiHeader {String} Authorization Bearer token
- *
- */
