d75eb1c19d6af5c85baade4d058704b033293647
Augusto Soares <gutosoares@outlook.com>
2018-03-03 10:01:34 -0300

    Change to single quote.

--- src/modules/realties/routes/index.ts
+++ src/modules/realties/routes/index.ts
@@ -1,17 +1,17 @@
-import * as express from "express";
+import * as express from 'express';
 
-import { createRealty } from "./realties-post";
-import { updateRealty } from "./realties-patch";
-import { deleteRealty } from "./realties-del";
-import { getAllRealties, getSingleRealty } from "./realties-get";
+import { createRealty } from './realties-post';
+import { updateRealty } from './realties-patch';
+import { deleteRealty } from './realties-del';
+import { getAllRealties, getSingleRealty } from './realties-get';
 
 const RealtiesRoutes = express.Router();
 
 RealtiesRoutes
-  .get("/", getAllRealties)
-  .get("/:id", getSingleRealty)
-  .patch("/update/:id", updateRealty)
-  .post("/", createRealty)
-  .delete("/:id", deleteRealty);
+  .get('/', getAllRealties)
+  .get('/:id', getSingleRealty)
+  .patch('/update/:id', updateRealty)
+  .post('/', createRealty)
+  .delete('/:id', deleteRealty);
 
 export default RealtiesRoutes;
