3749ed6a7fc348ee9166ccb7861da9808f4b181c
Augusto Soares <gutosoares@outlook.com>
2018-03-03 09:24:41 -0300

    Set and config patch method for Realty module.

---
+++ src/modules/realties/routes/realties-patch.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { RealtySchema } from '../models/realty-schema';
+
+export const updateRealty = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const update = { $set: req.body };
+    const options = { new: true };
+
+    const realty = await RealtySchema.findByIdAndUpdate(req.params.id, update, options);
+    res.status(200).json({
+      status: 200,
+      message: `Realty with id ${req.params.id} successfully updated!`,
+      data: realty
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      status: 400,
+      message: error.message
+    });
+  }
+  next();
+};
