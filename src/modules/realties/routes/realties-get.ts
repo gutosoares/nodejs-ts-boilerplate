36a7759c41826c0e4d95ef7239a74a3f606fac94
Augusto Soares <gutosoares@outlook.com>
2018-03-03 09:25:06 -0300

    Set and config gets methods for Realty module.

---
+++ src/modules/realties/routes/realties-get.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import { RealtySchema } from '../models/realty-schema';
+
+export const getAllRealties = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { limit, offset } = req.query;
+    const realties = await RealtySchema.find().skip(offset).limit(limit);
+
+    res.status(200).json({
+      status: 200,
+      message: "Realties successfully loaded!",
+      data: realties
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      status: 400,
+      message: error.message
+    });
+  }
+  next();
+};
+
+export const getSingleRealty = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const realty = await RealtySchema.findOne({ _id: id });
+
+    res.status(200).json({
+      status: 200,
+      message: "Realty successfully loaded!",
+      data: realty
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      status: 400,
+      message: error.message
+    });
+  }
+  next();
+};
